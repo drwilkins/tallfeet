48232af0f10d2e4788c19c566da02bb54b061512
drwilkins <mrwilkins06@gmail.com>
2017-09-01 14:10:09 -0500

    Initiate app

---
+++ FootVHeight/app.R
@@ -0,0 +1,41 @@
+
+library(shiny);require(plotly);require(ggplot2)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      #import school data
+     x<-read.csv("footvheight.csv")
+    ggplot(x,aes(x=footsize,y=height,col=NULL))+geom_point()
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
