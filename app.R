0f79a969c802bb47ef67e48a78421c18f4d0b9e8
Matt Wilkins <mattwilkins@MacBook-Pro-3.local>
2020-02-11 22:06:40 -0600

    Major changes. Hopefully speedier. Adds equations.

--- app.R
+++ app.R
@@ -8,28 +8,29 @@ vals<-reactiveValues()
 isolate(vals$grouping<-NULL)
 observe(vals$xrange<-input$xrange)
 observe(vals$yrange<-input$yrange)
-observe(vals$x2<-subset(x,footsize>vals$xrange[1]&footsize<vals$xrange[2]&height>vals$yrange[1]&height<vals$yrange[2]) )
-
+observe(vals$x2<-subset(x,footsize>=input$xrange[1]&footsize<=input$xrange[2]&height>=input$yrange[1]&height<=input$yrange[2]) )
+isolate(vals$x2_group<-list(vals$x2))
 
 observeEvent(input$classgroup,
       {vals$grouping <- "class"
-      vals$x2_group=lapply(levels(vals$x2$class),FUN=function(x) {subset(vals$x2,class==x)})
-      names(vals$x2_group)<-levels(vals$x2$class)
       },ignoreNULL = F)
 
 #Start Plot def
 observe(vals$aes<-paste("x=footsize","y=height",paste0("fill=",ifelse(is.null(vals$grouping),"NULL",vals$grouping)),sep=",")) 
 
 observe(#create custom theme for axis formatting
-     vals$mytheme<-theme_linedraw()+theme(axis.text=element_text(size=16,margin = margin(r = 20,t=6) ),axis.title=element_text(size=18,face="bold"), strip.text.x = element_text(size = 15)))
+     vals$mytheme<-theme_linedraw()+theme(axis.text=element_text(size=16,margin = margin(r = 20,t=6) ),plot.title = element_text(size = 15),axis.title=element_text(size=18,face="bold"), strip.text.x = element_text(size = 15)))
 
 output$g <- renderPlot({
+  
   #function def  
   myplot<-function(data,x,y,grp,...){
       x<-enquo(x)
       y<-enquo(y)
       grp<-enquo(grp)
-      g<-ggplot(data,aes(!!x,!!y,col=!!grp))+vals$mytheme+theme(aspect.ratio=1)+xlab("Foot Size (in)")+ylab("Height (in)")+ggtitle("Combined Class Data")+
+      g<-ggplot(data,aes(!!x,!!y,col=!!grp))+vals$mytheme+theme(aspect.ratio=1)+xlab("Foot Length (in)")+ylab("Height (in)")+ggtitle("Combined Class Data")+
+        {if(input$zero){expand_limits(x=0,y=0)}else{}}+
+        {if(input$same){expand_limits(x=vals$yrange[2])}else{}}+
         {if(input$fitline==T){geom_smooth(method="lm",aes(group=1),se=F,linetype="dashed",size=1.2,col="black",show.legend=F)}else{}
         }
       
@@ -46,7 +47,7 @@ output$g <- renderPlot({
     
     
   #make initial plot (no trend line, color depends on grouping)
-    # g<-ggplot(vals$x2,aes_string(vals$aes))+geom_point(size=3,pch=21,col="#202020",stroke=.9)+vals$mytheme+theme(aspect.ratio=1)+xlab("Foot Size (cm)")+ylab("Height (cm)")+ggtitle("Combined Class Data")
+    # g<-ggplot(vals$x2,aes_string(vals$aes))+geom_point(size=3,pch=21,col="#202020",stroke=.9)+vals$mytheme+theme(aspect.ratio=1)+xlab("Foot Length (cm)")+ylab("Height (cm)")+ggtitle("Combined Class Data")
     # 
     # #if user wants to fit lines, add smoother to plot
     # if(input$fitline==1){
@@ -84,7 +85,13 @@ output$g <- renderPlot({
 
 # Define summary table   
     output$summary<-renderTable({
+      # vals$x2_group <- lapply(levels(vals$x2$class),FUN=function(x) 
+      #                     {subset(vals$x2,class==x)})
+      # names(vals$x2_group)<-levels(vals$x2$class)
+      
     s<-data.frame(Class="Overall",N=nrow(vals$x2),MeanFootLn=mean(vals$x2$footsize,na.rm=T),MedianFootLn=median(vals$x2$footsize,na.rm=T),ModeFootLn=getmode(vals$x2$footsize),MeanHeight=mean(vals$x2$height,na.rm=T),MedianHeight=median(vals$x2$height,na.rm=T),ModeHeight=getmode(vals$x2$height))
+ 
+    if(input$fitline){s$LineEQ=ymxb(vals$x2,"height~footsize")}
 
     if(input$classgroup==1){
       classes<-sort(unique(vals$x2$class))
@@ -93,11 +100,16 @@ output$g <- renderPlot({
         currclass<-classes[i]
         cdf<-subset(vals$x2,class==currclass)
         cl<-data.frame(Class=as.character(currclass),N=nrow(cdf),MeanFootLn=mean(cdf$footsize,na.rm=T),MedianFootLn=median(cdf$footsize,na.rm=T),ModeFootLn=getmode(cdf$footsize),MeanHeight=mean(cdf$height,na.rm=T),MedianHeight=median(cdf$height,na.rm=T),ModeHeight=getmode(cdf$height))
+        
         tmp<-rbind(tmp,cl)
       }
+      if(input$fitline){
+        tmp$LineEQ=sapply(levels(vals$x2$class),function(clase){ df=subset(vals$x2,class==clase); ymxb(df,form="height~footsize")})
+        }
       s<-rbind(tmp,s)
+      
     }
-    names(s)<-c("Class","N","Mean Foot Len","Median Foot Len","Mode Foot Len","Mean Height","Median Height","Mode Height")  
+  names(s)[1:8]<-c("Class","N","Mean Foot Len","Median Foot Len","Mode Foot Len","Mean Height","Median Height","Mode Height")  
     s
     })#End summary table
 
@@ -116,7 +128,7 @@ output$g <- renderPlot({
    }#End histogram definitions
 
 #Add custom color & legend
-   foothist2<-foothist+scale_color_manual(name="Stats",values=c(Mean="#FFA630",Median="#2E5077",Mode="#E8437F"))+guides(col=guide_legend(override.aes=list(linetype=c("dashed","dashed","dotted"))))+xlab("Foot Size (cm)")
+   foothist2<-foothist+scale_color_manual(name="Stats",values=c(Mean="#FFA630",Median="#2E5077",Mode="#E8437F"))+guides(col=guide_legend(override.aes=list(linetype=c("dashed","dashed","dotted"))))+xlab("Foot Length (cm)")
    heighthist2<-heighthist+scale_color_manual(name="Stats",values=c(Mean="#FFA630",Median="#2E5077",Mode="#E8437F"))+guides(col=guide_legend(override.aes=list(linetype=c("solid","dashed","dotted"))))+xlab("Height (cm)")
 
    #extract legend
@@ -132,8 +144,8 @@ output$g <- renderPlot({
 
 #Reset button backend
     observeEvent(input$rst,{
-      updateSliderInput(session,"xrange",min = min(x$footsize),max = max(x$footsize),value = c(min(x$footsize),max(x$footsize)))
-      updateSliderInput(session,"yrange",min = min(x$height),max = max(x$height),value = c(min(x$height),max(x$height)) )
+      updateSliderInput(session,"xrange",min = min(x$footsize,na.rm=T),max = max(x$footsize,na.rm=T),value = c(min(x$footsize,na.rm=T),max(x$footsize,na.rm=T)))
+      updateSliderInput(session,"yrange",min = min(x$height,na.rm=T),max = max(x$height,na.rm=T),value = c(min(x$height,na.rm=T),max(x$height,na.rm=T)) )
       updateCheckboxInput(session,"classgroup",value=FALSE)
       updateCheckboxInput(session,"fitline",value=FALSE)
       updateCheckboxInput(session,"hist",value=FALSE)
@@ -152,21 +164,24 @@ output$g <- renderPlot({
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Exploring Class Height and Foot Size Data"),
+   titlePanel("Exploring Class Height and Foot Length Data"),
    
    # Sidebar with a slider input for number of bins 
-   sidebarLayout(
+fluidRow(
+  sidebarLayout(
       sidebarPanel(width=3,
          sliderInput("xrange",
-                     "Foot Size Range (cm)",min = min(x$footsize),max = max(x$footsize),value = c(min(x$footsize),max(x$footsize))),
+                     "Foot Length Range (in)",min = min(x$footsize,na.rm=T),max = max(x$footsize,na.rm=T),value = c(min(x$footsize,na.rm=T),max(x$footsize,na.rm=T))),
         sliderInput("yrange",
-                     "Height Range (cm)",
-                     min = min(x$height),
-                     max = max(x$height),
-                     value = c(min(x$height),max(x$height))),
+                     "Height Range (in)",
+                     min = min(x$height,na.rm=T),
+                     max = max(x$height,na.rm=T),
+                     value = c(min(x$height,na.rm=T),max(x$height,na.rm=T))),
         checkboxInput("classgroup","Color by Class?",FALSE),
         checkboxInput("fitline","Fit Lines?",FALSE),
         checkboxInput("hist","Show Histograms?",FALSE),
+        checkboxInput("zero","Show the Origin (0,0)?",FALSE),
+        checkboxInput("same","X and Y on Same Scale?",FALSE),
         actionButton("rst","Reset")
       ),
       
@@ -179,6 +194,7 @@ ui <- fluidPage(
   fluidRow(column(8,tableOutput("summary"),offset=0)),
   fluidRow(plotOutput("histplot"),height=600)
     
+)
 )
 
 
--- global.R
+++ global.R
@@ -6,8 +6,18 @@
    uniqv[which.max(tabulate(match(v, uniqv)))]
    }
    
+   #extract line equation
+ymxb<-function(data,form){
+   mod<-lm(as.formula(form),data=data)
+   b=round(coef(mod)[1],2)
+   m=round(coef(mod)[2],2)
+   return(paste0("y= ",m,"x + ",b))
+}
+   
    #import school data
-x<-read.csv("data/footvheight.csv")
+x<-read.csv("data/footvheight_Davis 2020.csv")
 names(x)<-tolower(names(x)) #lower case everything
 names(x)<-gsub("period","class",names(x),fixed=T)#if period is used instead of class
 x$class<-factor(x$class)
+x$height<-as.numeric(x$height)
+x$footsize<-as.numeric(x$footsize)
